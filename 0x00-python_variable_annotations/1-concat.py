dd6703959ba7eda20c14db4f4dafb606410c5d5c
Elksass <qsasomar@gmail.com>
2024-05-03 03:32:43 +0300

    Task 1 Basic annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+"""_
+    Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Type-annotated function concat that takes a string str1 and a
+        string str2 as arguments and returns a concatenated string
+    """
+    return "{}{}".format(str1, str2)

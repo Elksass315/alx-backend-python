dce550bdfa261424c6166eec2ad7e1614c4a444e
Elksass <qsasomar@gmail.com>
2024-05-08 05:29:48 +0300

    return and var annotated

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
+'''Task 0's module.
+'''
 import asyncio
 import random
 
-"""
-    Write a coroutine called wait_random that takes in an integer max_delay and returns a random delay between 0 and max_delay. Use the random module.
-    """
-    
-async def wait_random(max_delay = 10):
-    """
-        async func delay for random time
-        max_delay: max delay time
-        return: the time delayed
-    """
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time

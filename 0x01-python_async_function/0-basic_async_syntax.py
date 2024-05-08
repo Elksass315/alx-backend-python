b36f9abab3541da102436f9025b3e5cf74428398
Elksass <qsasomar@gmail.com>
2024-05-08 05:21:26 +0300

    Basic Async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+"""
+    Write a coroutine called wait_random that takes in an integer max_delay and returns a random delay between 0 and max_delay. Use the random module.
+    """
+    
+async def wait_random(max_delay = 10):
+    """
+        async func delay for random time
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
